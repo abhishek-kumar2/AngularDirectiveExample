a9a4906f3d609967022cbc5da7e993b772750726
Abhishek Kumar <abhishek.kumar2@outlook.com>
2016-01-07 20:51:44 +0530

    Directive basic plus behavior examples

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AngularDirectiveExample
 a full functional angular directive example
+
+directiveBasicBehavior.html	  Directive basic behavior examples
+directiveRestrictions.html	  Directive restrict examples
+shoppingCart.html	          Shopping cart example using directive
+simpleDirective.html	      Simple directive example
--- js/directiveBasicBehavior.js
+++ js/directiveBasicBehavior.js
@@ -1,14 +1,16 @@
 /**
  * Created by abhishekkumar on 07/01/16.
  */
-var app = angular.module("basicBehaviourApp", []);
+var app = angular.module("basicBehaviorApp", []);
 
 app.directive("directiveBasicBehavior", function(){
-   return function(scope, element){
+   return function(scope, element, attrs){
        element.bind("mouseenter", function(){
+           element.addClass(attrs.directiveBasicBehavior);
            console.log("mouseenter");
        });
        element.bind("mouseleave", function(){
+           element.removeClass(attrs.directiveBasicBehavior);
            console.log("mouseleave");
        });
    }
